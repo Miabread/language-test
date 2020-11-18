4301325b5ae916caa42e99a0e2665e9713634df4
jamesBeeProg <jamesBeeProg@gmail.com>
2020-11-18 12:42:36 -0500

    Add comments to codegen

--- compiler/src/codegen/mod.rs
+++ compiler/src/codegen/mod.rs
@@ -7,6 +7,7 @@ use {
 };
 
 pub fn codegen(input: ParseResult) -> Result<Vec<u8>, BackendError> {
+    // Create a module using host configuation
     let mut module = {
         let isa =
             isa::lookup(target_lexicon::HOST)?.finish(settings::Flags::new(settings::builder()));
@@ -15,33 +16,41 @@ pub fn codegen(input: ParseResult) -> Result<Vec<u8>, BackendError> {
         Module::<ObjectBackend>::new(builder)
     };
 
-    let mut ctx = module.make_context();
+    // Context objects
+    let mut context = module.make_context();
     let mut builder_context = FunctionBuilderContext::new();
 
+    // Create a function signature and attach it
     let mut signature = module.make_signature();
     signature.returns.push(AbiParam::new(types::I32));
     let id = module.declare_function(&input.name, Linkage::Export, &signature)?;
-    ctx.func.signature = signature;
+    context.func.signature = signature;
 
+    // Build the function
     {
-        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_context);
+        let mut builder = FunctionBuilder::new(&mut context.func, &mut builder_context);
 
+        // Setup the EBB
         let block = builder.create_block();
         builder.append_block_params_for_function_params(block);
         builder.switch_to_block(block);
         builder.seal_block(block);
 
+        // Fill in a const and ret instruction
         let result = builder.ins().iconst(types::I32, input.body as i64);
         builder.ins().return_(&[result]);
 
+        // Finialize the EBB
         builder.seal_all_blocks();
         builder.finalize();
     }
 
-    module.define_function(id, &mut ctx, &mut NullTrapSink {})?;
-    module.clear_context(&mut ctx);
+    // Finialize the function
+    module.define_function(id, &mut context, &mut NullTrapSink {})?;
+    module.clear_context(&mut context);
     module.finalize_definitions();
 
+    // Return bytecode
     Ok(module.finish().emit()?)
 }
 

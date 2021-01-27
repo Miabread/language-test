bfd77526a86dd7207561b26fa5f6954fe356538c
jamesBeeProg <jamesbeeprog@gmail.com>
2021-01-27 14:52:51 -0500

    Have the compiler emit a binary

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,5 +1,43 @@
-pub mod ty;
+use {
+    clap::Clap,
+    compiler::{compile, File},
+    std::{env, fs, io::Write},
+};
+
+#[derive(Debug, Clone, Clap)]
+#[clap(version = "0.1.0", author = "jamesBeeProg <jamesBeeProg@gmail.com>")]
+struct Settings {
+    name: String,
+    number: u8,
+    #[clap(default_value = "../indev/output.o")]
+    output: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let cwd = env::current_dir().expect("couldn't get current dir");
+    let settings = Settings::parse();
+
+    // let input = fs::read_to_string(cwd.join(settings.input)).expect("couldn't read source file");
+
+    let input = File {
+        name: &settings.name,
+        number: settings.number,
+    };
+
+    let compiled = match compile(&input) {
+        Ok(compiled) => compiled,
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            return;
+        }
+    };
+
+    fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(cwd.join(settings.output))
+        .unwrap()
+        .write_all(&compiled)
+        .unwrap();
 }

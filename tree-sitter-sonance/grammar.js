18e8ece47d54c7990167750af35672585733ce83
jamesBeeProg <jamesbeeprog@gmail.com>
2021-01-27 17:51:19 -0500

    Complete phase 1: tree sitter

--- tree-sitter-sonance/grammar.js
+++ tree-sitter-sonance/grammar.js
@@ -1,3 +1,39 @@
+const tokens = {
+    // Regex
+    identifier: /[a-zA-Z_]\w*/,
+    number: /\d+/,
+
+    // Keywords
+    func: 'func',
+    import: 'import',
+    external: 'external',
+
+    // Groups
+    open_paren: '(',
+    close_paren: ')',
+    open_block: '{',
+    close_block: '}',
+
+    // Punctuation
+    arrow: '->',
+    semi: ';',
+    colon: ':',
+    at: '@',
+};
+
+const
+    sepBy = (sep, rule) => optional(sepBy1(sep, rule)),
+    sepBy1 = (sep, rule) => seq(
+        rule,
+        repeat(seq(sep, rule)),
+        optional(sep),
+    ),
+
+    wrap = (start, end) => (rule = '') => seq(start, rule, end),
+    parens = wrap('(', ')'),
+    block = wrap('{', '}'),
+    angles = wrap('<', '>');
+
 module.exports = grammar({
     name: 'sonance',
     extras: $ => [
@@ -6,10 +42,66 @@ module.exports = grammar({
     ],
 
     rules: {
-        source_file: $ => seq('func', $.identifier, '(', ')', '->', 'U8', '{', $.number, '}', ';'),
+        source_file: $ => repeat($._item),
 
-        identifier: $ => /[a-zA-Z_]\w*/,
-        number: $ => /\d+/,
+        _item: $ => seq(
+            repeat($.attribute),
+            choice(
+                $.import_item,
+                $.func_item,
+            ),
+            tokens.semi,
+        ),
+
+                attribute: $ => seq(
+            tokens.at,
+            $.identifier,
+            parens(),
+        ),
+
+        import_item: $ => seq(
+           tokens.import,
+           block(repeat($._item)),
+        ),
+
+        func_item: $ => seq(
+            tokens.func,
+            field('name', $.identifier),
+            field('parameters', $.parameter_list),
+            tokens.arrow,
+            field('return_type', $._type),
+            optional(field('body', $.block)),
+        ),
+
+        parameter_list: $ => parens(sepBy(',', $.parameter)),
+
+        parameter: $ => seq(
+            field('name', $.identifier),
+            tokens.colon,
+            field('type', $._type),
+        ),
+
+        _type: $ => choice(
+            $.identifier,
+        ),
+
+        block: $ => block(sepBy(';', $._expression)),
+
+        _expression: $ => choice(
+            $.number,
+            $.call_expression,
+        ),
+
+        call_expression: $ => seq(
+            optional(field('leading', $._expression)),
+            field('name', $.identifier),
+            field('arguments', $.argument_list),
+        ),
+
+        argument_list: $ => parens(sepBy(',', $._expression)),
+
+        identifier: $ => tokens.identifier,
+        number: $ => tokens.number,
 
         comment: $ => token(choice(
             seq('//', /.*/),

e935d9c0d4359f3a53d0f6976674726ed65a1755
jamesBeeProg <jamesbeeprog@gmail.com>
2021-03-06 20:49:15 -0500

    Implement basic parser

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -14,7 +14,10 @@ pub fn run(source: &str) {
     let parsed = parser::parse(tokens.into_iter());
 
     match parsed {
-        Ok(f) => println!("{:?}", f),
-        Err(e) => error::Reporter::new(source).report(&[e.into()]),
+        Ok(parsed) => println!("{:?}", parsed),
+        Err(errors) => {
+            let errors: Vec<_> = errors.into_iter().map(Into::into).collect();
+            error::Reporter::new(source).report(&errors);
+        }
     }
 }
--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -1,33 +1,55 @@
 use crate::token::{Token, TokenKind};
 
-#[derive(Debug, Clone)]
-pub enum Expression {
-    Number(f64),
-    Debug(Box<Expression>),
-}
-
 pub fn parse<'src>(
     mut source: impl Iterator<Item = Token<'src>>,
-) -> Result<Expression, ParseError<'src>> {
-    let token = source.next().unwrap();
-    Ok(match token.kind {
-        TokenKind::Number(num) => Expression::Number(num),
-        TokenKind::DebugKeyword => Expression::Debug(Box::new(parse(source)?)),
-        _ => return Err(ParseError::UnimplementedToken { token }),
-    })
+) -> Result<Vec<f64>, Vec<ParseError<'src>>> {
+    let mut result = Vec::new();
+    let mut errors = Vec::new();
+
+    while let Some(token) = source.next() {
+        match token.kind {
+            TokenKind::Number(number) => result.push(number),
+            TokenKind::Semicolon => continue,
+            _ => {
+                errors.push(ParseError::Unimplemented { token });
+                continue;
+            }
+        }
+
+        if let Some(token) = source.next() {
+            if let TokenKind::Semicolon = token.kind {
+                continue;
+            } else {
+                errors.push(ParseError::ExpectedSemicolon { token });
+                continue;
+            }
+        } else {
+            break;
+        };
+    }
+
+    if errors.is_empty() {
+        Ok(result)
+    } else {
+        Err(errors)
+    }
 }
 
 #[derive(Debug, Clone)]
 pub enum ParseError<'src> {
-    UnimplementedToken { token: Token<'src> },
+    ExpectedSemicolon { token: Token<'src> },
+    Unimplemented { token: Token<'src> },
 }
 
 impl<'src> From<ParseError<'src>> for crate::error::Citation {
     fn from(error: ParseError<'src>) -> Self {
         use crate::error::Citation;
         match error {
-            ParseError::UnimplementedToken { token } => {
-                Citation::error("Unimplemented token type".to_owned()).span(token.span, None)
+            ParseError::ExpectedSemicolon { token } => {
+                Citation::error("Expected semicolon".to_owned()).span(token.span, None)
+            }
+            ParseError::Unimplemented { token } => {
+                Citation::error("Unimplemented".to_owned()).span(token.span, None)
             }
         }
     }
